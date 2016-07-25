cd7eb3a6d4924ac048062caeaf139e068c834ae6
Daniel Hand <email@danielhand.io>
2016-07-25 21:32:26 +0100

    Remove WooCommerce tab temporarily

--- admin/partials/wpto-admin-display.php
+++ admin/partials/wpto-admin-display.php
@@ -16,9 +16,9 @@
 	<?php	if ( defined('WPSEO_VERSION') ) { ?>
 	<a href="#yoast" class="nav-tab"><?php _e('Yoast Seo', $this->plugin_name);?></a>
 <?php }
-if ( class_exists( 'WooCommerce' ) ) {?>
+/*if ( class_exists( 'WooCommerce' ) ) {?>
 <a href="#wc" class="nav-tab"><?php _e('WooCommerce', $this->plugin_name);?></a>
-<?php } ?>
+<?php  } */ ?>
 <a href="#contact" class="nav-tab"><?php _e('Contact', $this->plugin_name);?></a>
   </h2>
 
