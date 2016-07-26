718c0a5076ef801130c18b254fdb001fa30d0e66
Daniel Hand <email@danielhand.io>
2016-07-26 10:38:22 +0000

    Moved WC settings code into the correct tab

--- admin/partials/wpto_wc.php
+++ admin/partials/wpto_wc.php
@@ -12,12 +12,28 @@
 	<h2><?php esc_attr_e( 'WooCommerce', $this->plugin_name ); ?></h2>
         <p><?php _e('To Add Yoast Options', $this->plugin_name);?></p>
 
-				<fieldset>
-					<legend class="screen-reader-text"><span><?php _e('Remove Yoast Information', $this->plugin_name);?></span></legend>
-					<label for="<?php echo $this->plugin_name;?>-remove_wc_price_slider">
-						<input type="checkbox" id="<?php echo $this->plugin_name;?>-remove_wc_price_slider" name="<?php echo $this->plugin_name;?>[remove_wc_price_slider]" value="1" <?php checked($remove_wc_price_slider, 1);?>/>
-						<span><?php esc_attr_e('Remove Yoast Information', $this->plugin_name);?></span>
-					</label>
-				</fieldset>
+			<?php
+$wc_dequeue_scripts = array();
+$wc_dequeue_scripts[101] = array(
+    "id" => "wc_cart_fragments",
+    "title" => "Remove wc-cart-fragments.js",
+    "description" => "Donec ullamcorper nulla non metus auctor fringilla.",   
+);
+
+if(!empty($wc_dequeue_scripts)) {
+foreach($wc_dequeue_scripts as $wc_dequeue_script) {
+?>
+	
+		<fieldset>
+			<legend class="screen-reader-text"><span><?php _e('Remove Pingbacks globally', $this->plugin_name);?></span></legend>
+			<label for="<?php echo $this->plugin_name;?>-wc_cart_fragments">
+				<input type="checkbox" id="<?php echo $this->plugin_name;?>-<?php echo $wc_dequeue_script["id"] ?>" name="<?php echo $this->plugin_name;?>[wc_cart_fragments]" value="1" <?php checked($wc_cart_fragments, 1);?>/>
+				<span><?php echo $wc_dequeue_script["title"] ?></span>
+			</label>
+		</fieldset>
+<?php
+}
+}
+?>
 
 							</div>
