d437f0a9e8bc0e4b304cde60303aa9d3d0a66248
Daniel Hand <email@danielhand.io>
2016-07-28 19:32:21 +0100

    Moved checkboxes outside of label tag to allow for better control

--- admin/partials/wpto_yoast.php
+++ admin/partials/wpto_yoast.php
@@ -15,11 +15,10 @@
 				<div class="options-box">
 					<legend class="screen-reader-text"><span><?php _e('Remove Yoast Information', $this->plugin_name);?></span></legend>
 					<label for="<?php echo $this->plugin_name;?>-remove_yoast_information">
-						<input type="checkbox" id="<?php echo $this->plugin_name;?>-remove_yoast_information" name="<?php echo $this->plugin_name;?>[remove_yoast_information]" value="1" <?php checked($remove_yoast_information, 1);?>/>
 						<h3><?php esc_attr_e('Remove Yoast Information', $this->plugin_name);?></h3>
 						<p>Remove the comments and version number left by Yoast SEO in your front-end HTMl.</p>
-
 					</label>
+					<input type="checkbox" id="<?php echo $this->plugin_name;?>-remove_yoast_information" name="<?php echo $this->plugin_name;?>[remove_yoast_information]" value="1" <?php checked($remove_yoast_information, 1);?>/>
 				</div>
 
 </div>
