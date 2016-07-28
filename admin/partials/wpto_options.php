332d354b821f7a814abaead8bbd648ee9f7e8899
Daniel Hand <email@danielhand.io>
2016-07-28 16:32:27 +0000

    Started to enter better descriptions for each item

--- admin/partials/wpto_options.php
+++ admin/partials/wpto_options.php
@@ -15,7 +15,8 @@
 		<legend class="screen-reader-text"><span><?php _e('Remove CSS and JS files query strings', $this->plugin_name);?></span></legend>
 		<label for="<?php echo $this->plugin_name;?>-css_js_versions">
 			<input type="checkbox" id="<?php echo $this->plugin_name;?>-css_js_versions" name="<?php echo $this->plugin_name;?>[css_js_versions]" value="1" <?php checked($css_js_versions, 1);?>/>
-			<span><?php esc_attr_e('Remove CSS and JS versions', $this->plugin_name);?></span>
+			<h3><?php esc_attr_e('Remove CSS and JS versions', $this->plugin_name);?></h3>
+			<p>Resources with a “?” or “&” in the URL can not always be cached correctly. Removing these query strings can improve the performance of your WordPress site.</p>
 		</label>
 	</fieldset>
 
