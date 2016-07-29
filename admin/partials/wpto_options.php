be8a6516570b0f57364adefadb48f3bcf3328354
Daniel Hand <email@danielhand.io>
2016-07-29 13:51:39 +0000

    Remove old code from settings page

--- admin/partials/wpto_options.php
+++ admin/partials/wpto_options.php
@@ -15,15 +15,14 @@
 <h2>Theme Links</h2>
 
 <!-- Remove oEmbed -->
-<div class="options-box remove_oembed">
+<div class="options-box">
 	<legend class="screen-reader-text"><span><?php _e('Remove OEmbed Links', $this->plugin_name);?></span></legend>
 	<label for="<?php echo $this->plugin_name;?>-remove_oembed">
 		<h3><?php esc_attr_e('Remove OEmbed Links', $this->plugin_name);?></h3>
 		<p>OEmbed provides an easy way to embed content from one site to another. If you do not need it, disable it here.</p>
 	</label>
-	<div class="options-checkbox remove_oembed">
+	<div class="options-checkbox">
 	<input type="checkbox" id="<?php echo $this->plugin_name;?>-remove_oembed" name="<?php echo $this->plugin_name;?>[remove_oembed]" value="1" <?php checked($remove_oembed, 1);?>/>
-	<span class="checkbox-disc" ></span>
 </div>
 </div>
 
@@ -34,19 +33,19 @@
 		<h3><?php esc_attr_e('Remove RSD Link', $this->plugin_name);?></h3>
 		<p>Are you editing your WordPress blog using your browser? Then you are not using a blog client and this link can probably be removed by disabling it here.</p>
 	</label>
-	<div class="options-checkbox remove_rsd_link">
+	<div class="options-checkbox">
 	<input type="checkbox" id="<?php echo $this->plugin_name;?>-remove_rsd_link" name="<?php echo $this->plugin_name;?>[remove_rsd_link]" value="1" <?php checked($remove_rsd_link, 1);?>/>
 </div>
 </div>
 
 <!-- Remove RSS Feeds -->
-<div class="options-box remove_rss_feed">
+<div class="options-box">
 	<legend class="screen-reader-text"><span><?php _e('Remove RSS feeds', $this->plugin_name);?></span></legend>
 	<label for="<?php echo $this->plugin_name;?>-remove_rss_feed">
 		<h3><?php esc_attr_e('Remove RSS feeds', $this->plugin_name);?></h3>
 		<p>Are you using RSS feeds? If not, you can turn them off here.</p>
 	</label>
-	<div class="options-checkbox remove_rss_feed">
+	<div class="options-checkbox">
 	<input type="checkbox" id="<?php echo $this->plugin_name;?>-remove_rss_feed" name="<?php echo $this->plugin_name;?>[remove_rss_feed]" value="1" <?php checked($remove_rss_feed, 1);?>/>
 </div>
 </div>
